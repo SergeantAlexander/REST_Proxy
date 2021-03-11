bc16b3bdbe5abe1340f33c543e8d2e1a15012206
ISultanov <ISultanov@luxoft.com>
2021-03-11 12:57:01 +0300

    Second commit

---
+++ src/test/java/ru/sergeantalexander/restproxy/RestproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergeantalexander.restproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
